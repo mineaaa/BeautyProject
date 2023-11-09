7b521adb74c687ed419b934a8b34d838e27630bf
Minea <117983209+mineaaa@users.noreply.github.com>
2023-11-09 14:26:05 +0200

    preauthorize hommelit

--- src/main/java/hh/sof03/harjoitustyo/web/KategoriaController.java
+++ src/main/java/hh/sof03/harjoitustyo/web/KategoriaController.java
@@ -3,6 +3,7 @@ package hh.sof03.harjoitustyo.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,36 +29,42 @@ public class KategoriaController {
     }
 
     @RequestMapping(value = "/addtype", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String addProductType(Model model) {
         model.addAttribute("kategoria", new Kategoria(null));
         return "addkategoria";
     }
 
     @RequestMapping(value = "/savetype", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String saveProductType(Kategoria kategoria) {
         kategoriaRepository.save(kategoria);
         return "redirect:/kategorialist";
     }
 
     @RequestMapping(value = "/deletecategory/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String deleteCategory(@PathVariable("id") Long id, Model model) {
         kategoriaRepository.deleteById(id);
         return "redirect:/kategorialist";
     }
 
     @RequestMapping(value = "/editcategory/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String editCategory(@PathVariable("id") Long id, Model model) {
         model.addAttribute("kategoria", kategoriaRepository.findById(id));
         return "editkategoria";
     }
 
     @RequestMapping(value = "/saveeditcategory", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String saveEditCategory(@ModelAttribute("kategoria") Kategoria kategoria) {
         kategoriaRepository.save(kategoria);
         return "redirect:/kategorialist";
     }
 
     @RequestMapping(value = "/rest/kategoria/getCategory", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public @ResponseBody List<Kategoria> getCategory(@RequestParam("tuotetyyppi") String tuotetyyppi) {
         return kategoriaRepository.findByTuotetyyppi(tuotetyyppi);
     }
--- src/main/java/hh/sof03/harjoitustyo/web/KynsilakkaController.java
+++ src/main/java/hh/sof03/harjoitustyo/web/KynsilakkaController.java
@@ -3,6 +3,7 @@ package hh.sof03.harjoitustyo.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -32,12 +33,14 @@ public class KynsilakkaController {
     }
 
     @RequestMapping(value = "/deletenailpolish/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String deleteNailPolish(@PathVariable("id") Long id, Model model) {
         kynsilakkaRepository.deleteById(id);
         return "redirect:../kynsilakkalist";
     }
 
     @RequestMapping(value = "/addnailpolish", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String addNailPolsih(Model model) {
         model.addAttribute("kynsilakka", new Kynsilakka());
         model.addAttribute("kategoriat", kategoriaRepository.findAll());
@@ -45,12 +48,14 @@ public class KynsilakkaController {
     }
 
     @RequestMapping(value = "/savenailpolish", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String saveNailpolish(Kynsilakka kynsilakka) {
         kynsilakkaRepository.save(kynsilakka);
         return "redirect:/kynsilakkalist";
     }
 
     @RequestMapping(value = "/editnailpolish/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String editNailPolish(@PathVariable("id") Long id, Model model) {
         model.addAttribute("nailpolish", kynsilakkaRepository.findById(id));
         model.addAttribute("kategoriat", kategoriaRepository.findAll());
@@ -58,12 +63,14 @@ public class KynsilakkaController {
     }
 
     @RequestMapping(value = "/saveeditnailpolish", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String editKynsilakkaSubmit(@ModelAttribute("nailpolish") Kynsilakka nailpolish) {
         kynsilakkaRepository.save(nailpolish);
         return "redirect:/kynsilakkalist";
     }
 
     @RequestMapping(value = "/rest/kynsilakka/getVäri", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public @ResponseBody List<Kynsilakka> getVäri(@RequestParam("lakkaVari") String lakkaVari) {
         return kynsilakkaRepository.findByLakkaVari(lakkaVari);
     }
